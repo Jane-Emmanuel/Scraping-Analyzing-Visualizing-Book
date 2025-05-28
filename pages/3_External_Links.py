37e1c41d7cae0ad158a9f36660ceecc867c2d6cc
Jane <janeemmanuel247@gmail.com>
2025-05-27 22:11:49 -0700

    Update 3_External_Links.py

--- pages/3_External_Links.py
+++ pages/3_External_Links.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.markdown("### 🔗 Useful Links")
 
-st.markdown("[🛠 View Source Code on GitHub](https://github.com/yourusername/yourrepo)")
+st.markdown("[🛠 View Source Code on GitHub](https://github.com/Jane-Emmanuel/Scraping-Analyzing-Visualizing-Book)")
 st.markdown("[📝 Read the Blog Post](https://yourblog.com/books-dashboard)")
