e4f59dbe693c344a8c216ad002b2e600298dd753
Jane <janeemmanuel247@gmail.com>
2025-05-27 22:07:58 -0700

    Create 3_External_Links.py

---
+++ pages/3_External_Links.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("### 🔗 Useful Links")
+
+st.markdown("[🛠 View Source Code on GitHub](https://github.com/yourusername/yourrepo)")
+st.markdown("[📝 Read the Blog Post](https://yourblog.com/books-dashboard)")
